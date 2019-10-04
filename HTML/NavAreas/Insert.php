e1d70d0dd415591b894ec3becabc8cf584d61e28
Jonathan <jsvitana@yahoo.com>
2019-10-04 09:52:48 -0400

    Finish Writing INSERT (add required fields)

--- HTML/NavAreas/Insert.php
+++ HTML/NavAreas/Insert.php
@@ -37,41 +37,49 @@
         <input type="radio" id="equipBut" onclick="openEquipment()" name="forms" />
     </form>
 
-    <form id="Finance" method=POST action="/PHP/Insert-Process.php">
-        <label name="type">Finance</label> <br>
-        <input type="text" name="fname"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/>
+    <form id="Finance" method="POST" action="/PHP/Insert-Process.php">
+        <h1>Finance</h1>
+        <input type="text" placeholder="Hay Finances" name="finHay" required/> <br>
+        <input type="text" placeholder="Cattle Finances" name="finCow" required/> <br>
+        <input type="text" placeholder="Repair Finances" name="finRep" required/> <br>
+        <input type="text" placeholder="Spring,Fall,Summer,Winter" name="finQuarter" required/> <br>
+        <input type="text" placeholder="Year" name="finYear" required/>
+
+        <input type="hidden" value="fin" name="type"/>
         <input type="submit" value="Submit"/>
     </form>
 
-    <form id="Hay" action="/PHP/Insert-Process.php">
+    <form id="Hay" method="POST" action="/PHP/Insert-Process.php">
         <h1>Hay</h1>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/>
-        <input type="submit"/>
+        <input type="text" placeholder="Round or Square" name="hayType"/> <br>
+        <input type="text" placeholder="Current Amount" name="hayCurr"/> <br>
+        <input type="text" placeholder="Sold Amount" name="haySol"/> <br>
+        <input type="text" placeholder="Fed Amount" name="hayFed"/>
+
+        <input type="hidden" value="hay" name="type"/>
+        <input type="submit" value="Submit"/>
     </form>
 
-    <form id="Cattle" action="/PHP/Insert-Process.php">
+    <form id="Cattle" method="POST" action="/PHP/Insert-Process.php">
         <h1>Cattle</h1>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/>
-        <input type="submit"/>
+        <input type="text" placeholder="Tag Number" name="cowTag"/> <br>
+        <input type="text" placeholder="Name" name="cowName"/> <br>
+        <input type="text" placeholder="Breed" name="cowBreed"/> <br>
+        <input type="text" placeholder="Color" name="cowColor"/>
+
+        <input type="hidden" value="cow" name="type"/>
+        <input type="submit" value="Submit"/>
     </form>
 
-    <form id="Equipment" action="/PHP/Insert-Process.php">
+    <form id="Equipment" method="POST" action="/PHP/Insert-Process.php">
         <h1>Equipment</h1>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/> <br>
-        <input type="text"/>
-        <input type="submit"/>
+        <input type="text" placeholder="Type (ex. Tractor)" name="equipType"/> <br>
+        <input type="text" placeholder="Make" name="equipMake"/> <br>
+        <input type="text" placeholder="Model" name="equipModel"/> <br>
+        <input type="text" placeholder="Year" name="equipYear"/>
+
+        <input type="hidden" value="equip" name="type"/>
+        <input type="submit" value="Submit"/>
     </form>
 
     <script>
--- PHP/Insert-Process.php
+++ PHP/Insert-Process.php
@@ -1,4 +1,63 @@
 <?php
-$name = $_REQUEST['type'];
-echo $name;
+// connect to database//////////////////////////////////////
+$dbhost = "127.0.0.1:3307"; //SET TO LOCALHOST
+$dbuser = "root";
+$dbpass = "";
+$db = "farm_database";
+
+//connect
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+////////////////////////////////////////////////////////////
+
+$type = $_REQUEST['type'];
+
+if($type == "fin") {
+    $finHay = $_REQUEST["finHay"];
+    $finCow = $_REQUEST["finCow"];
+    $finRep = $_REQUEST["finRep"];
+    $finQuarter = $_REQUEST["finQuarter"];
+    $finYear = $_REQUEST["finYear"];
+
+    $sql = "INSERT INTO farm_database.finances (financeHay, financeCattle, financeRepairs, quarter, year)
+            VALUES ('$finHay', '$finCow', '$finRep', '$finQuarter', '$finYear')";
+    $result = $conn->query($sql);
+}
+
+if($type == "hay") {
+    $hayType = $_REQUEST["hayType"];
+    $hayCurr = $_REQUEST["hayCurr"];
+    $haySol = $_REQUEST["haySol"];
+    $hayFed = $_REQUEST["hayFed"];
+
+    $sql = "INSERT INTO farm_database.hay";
+    $result = $conn->query($sql);
+}
+
+if($type == "cow") {
+    $cowTag = $_REQUEST["cowTag"];
+    $cowName = $_REQUEST["cowName"];
+    $cowBreed = $_REQUEST["cowBreed"];
+    $cowColor = $_REQUEST["cowColor"];
+
+    $sql = "INSERT INTO farm_database.cattle (idcattle, name, breed, color)
+            VALUES ('$cowTag','$cowName','$cowBreed','$cowColor')";
+    $result = $conn->query($sql);
+}
+
+if($type == "equip") {
+    $equipType = $_REQUEST["equipType"];
+    $equipMake = $_REQUEST["equipMake"];
+    $equipModel = $_REQUEST["equipModel"];
+    $equipYear = $_REQUEST["equipYear"];
+
+    $sql = "INSERT INTO farm_database.equipment";
+    $result = $conn->query($sql);
+}
+
+
 ?>
